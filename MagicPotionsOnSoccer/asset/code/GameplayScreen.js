16a9e08fa55a5712b49679f0575c4b0fc1fa9a6f
Francisco Narto Pereira Junior <narto_20@hotmail.com>
2014-07-29 17:56:00 -0300

    Atualização das Mecânicas do Jogo
    falta apenas deixar o item chave aleatório como a mudança de itens na
    TV.

--- MagicPotionsOnSoccer/asset/code/GameplayScreen.js
+++ MagicPotionsOnSoccer/asset/code/GameplayScreen.js
@@ -6,65 +6,72 @@
 	
 	p.name = "gameplayscreen";
 	p.gamMan;
+	
 	// Usando Tick;
 	p.tick;
+	p.tickCount = 0;
 	p.isActived = false;
 	
 	// Setup e Teardown.
 	p.setup;
 	p.tearDown;
 	
-	/*
+	p.background;
+	p.score = 0;
 	p.placarJog = 0;
-	p.placarIA = 0;	
-	p.score = 0;		
+	p.placarIA = 0;
 	p.erros = 0;
 	p.acertos = 0;
-	*/
 	
-	p.background;
+	p.TIMETODIE = 10; // 60 segundos
+	p.elapsedTime = 0;
+	
+	p.isGameOver = false;
+	
+	p.hudScore;
+	p.hudPlacar;
+	p.hudTime;
+	
+	p.gameItens = [];
+	p.itensPosX = [210, 388, 580];
+	p.itensPosY = [250, 370, 480];
+	p.itemChave;
+	
+	// Métodos.
+	p.cText;
+	p.cItem;
+	p.rigthClick;
+	p.wrongClick;
 		
 	p.Container_initialize = p.initialize;
 	p.initialize = function(gameplayManager){
 		p.gamMan = gameplayManager;
 		
 		this.background = new createjs.Bitmap("asset/images/background.png");
+		
+		p.hudScore = this.cText(this.score, 650, 80);
+		p.hudPlacar = this.cText(this.placarJog + "x" + this.placarIA, 90, 90);
+		p.hudTime = this.cText(this.TIMETODIE - this.elapsedTime, 420, 80);
+		
+		var index = 0;
+		var indAux = 0;
+		
+		for (index; index < 9; index++)
+		{
+			var indMod = index%3;
+			if (indMod == 0 && index != 0)
+			{
+				indAux++;
+			}
+			p.gameItens[p.gameItens.length] = this.cItem(index, this.itensPosX[indAux], this.itensPosY[indMod])
+		}
+		
+		// vish.
+		var item1 = this.cItem(0, 210, 250);
+		
 		/*
 		var isGameOver = false;
 		var msgFim = false;	
-		
-		var TIMETODIE = 60; 
-		
-		var hudFimDeJogo;
-		hudFimDeJogo = new createjs.Text("Acabou a partida!", "bold 48px Arial", "#000000");
-		hudFimDeJogo.maxWidth = 1000;
-		hudFimDeJogo.textAlign = "center";
-		hudFimDeJogo.x = stage.canvas.width/2;
-		hudFimDeJogo.y = stage.canvas.height/2;		
-		
-		var hudScore;
-		hudScore = new createjs.Text(score, "bold 24px Arial", "#000000");
-		hudScore.maxWidth = 1000;
-		hudScore.textAlign = "right";
-		hudScore.x = stage.canvas.width-50;
-		hudScore.y = 25;
-		//stage.addChild(hudScore);
-
-		var placarHud;
-		placarHud = new createjs.Text(""+placarJog+"x"+placarIA, "bold 24px Arial", "#000000");
-		placarHud.maxWidth = 1000;
-		placarHud.textAlign = "center";
-		placarHud.x = 100;
-		placarHud.y = 25;
-		//stage.addChild(placarHud);				
-		
-		var timeHud;
-		timeHud = new createjs.Text(TIMETODIE.toString(), "bold 24px Arial", "#000000");
-		timeHud.maxWidth = 1000;
-		timeHud.textAlign = "center";
-		timeHud.x = stage.canvas.width / 2;
-		timeHud.y = 25;
-		//stage.addChild(timeHud);			
 		
 		var ele0 = new createjs.Bitmap("asset/images/ele0.jpg");
 		ele0.x = 210;
@@ -78,55 +85,21 @@
 		ele1.addEventListener("click", onWrongClicked);
 		//stage.addChild(ele1);	
 		
-		var ele2= new createjs.Bitmap("asset/images/ele2.jpg");
-		ele2.x = 580;
-		ele2.y = 250;		
-		ele2.addEventListener("click", onWrongClicked);
-		//stage.addChild(ele2);	
-		
-		var ele3 = new createjs.Bitmap("asset/images/ele3.jpg");
-		ele3.x = 210;
-		ele3.y = 370;		
-		ele3.addEventListener("click", onWrongClicked);
-		//stage.addChild(ele3);	
-		
-		var ele4 = new createjs.Bitmap("asset/images/ele4.jpg");
-		ele4.x = 388;
-		ele4.y = 370;		
-		ele4.addEventListener("click", onWrongClicked);
-		//stage.addChild(ele4);	
-		
-		var ele5 = new createjs.Bitmap("asset/images/ele5.jpg");
-		ele5.x = 580;
-		ele5.y = 370;		
-		ele5.addEventListener("click", onWrongClicked);
-		//stage.addChild(ele5);	
-		
-		var ele6 = new createjs.Bitmap("asset/images/ele6.jpg");
-		ele6.x = 210;
-		ele6.y = 480;		
-		ele6.addEventListener("click", onWrongClicked);
-		//stage.addChild(ele6);	
-		
-		var ele7 = new createjs.Bitmap("asset/images/ele7.jpg");
-		ele7.x = 388;
-		ele7.y = 480;		
-		ele7.addEventListener("click", onWrongClicked);
-		//stage.addChild(ele7);	
-		
-		var ele8 = new createjs.Bitmap("asset/images/ele8.jpg");
-		ele8.x = 580;
-		ele8.y = 480;		
-		ele8.addEventListener("click", onWrongClicked);
-		//stage.addChild(ele8);	
-		
-		
 		//TODO
 		x = Math.floor((Math.random() * 660) + 100);
 		y = Math.floor((Math.random() * 100) + 1);
 		
 		*/
 		this.addChild(this.background);
+		this.addChild(this.hudScore);
+		this.addChild(this.hudPlacar);
+		this.addChild(this.hudTime);
+		
+		index = 0;
+		for (index; index < 9; index++)
+		{
+			this.addChild(p.gameItens[index]);
+		}
 	}
 	
 	p.tick = function(event)
@@ -134,14 +107,65 @@
 		// Implementando Tick;
 		if (p.isActived)
 		{
-		
+			p.tickCount++;
+			
+			if (p.tickCount >= 60)
+			{
+				p.elapsedTime++;
+				p.tickCount = 0;
+			}
+			
+			var	time = p.TIMETODIE - p.elapsedTime;
+			
+			if (p.isGameOver == false)
+			{
+				// Update Hud.
+				p.hudPlacar.text = p.placarJog + "x" + p.placarIA;
+				p.hudTime.text = time;
+				p.hudScore.text = p.score;
+			}
+			else
+			{
+				p.isActived = false;
+				
+				var msgFinal = "Você ";
+				if (p.placarJog > p.placarIA)
+				{
+					msgFinal += "venceu!";
+				}
+				else if (p.placarJog < p.placarIA)
+				{
+					msgFinal += "perdeu!";
+				}
+				else
+				{
+					msgFinal += "empatou!";
+				}
+				
+				alert(msgFinal);
+				
+				p.gamMan.loadScreen("titlescreen");			
+				
+			}
+			
+			if (time <= 0)
+			{
+				p.isGameOver = true;
+			}
 		}
 	}
 	
 	p.setup = function()
 	{
 		// Implementando setup.
+		p.score = 0;
+		p.placarJog = 0;
+		p.placarIA = 0;
+		p.erros = 0;
+		p.acertos = 0;		
+		p.elapsedTime = 0;
 		
+		p.tickCount = 0;
 		
 		p.isActived = true;
 	}
@@ -154,9 +178,37 @@
 		p.isActived = false;
 	}
 	
-	this.createScoreHud = function()
+	p.cText = function(text, x, y)
+	{ 
+		var crejsText;
+		
+		crejsText = new createjs.Text(text, "bold 36px Arial", "#000000");
+		crejsText.maxWidth = 1000;
+		crejsText.textAlign = "center";
+		crejsText.x = x;
+		crejsText.y = y;
+		
+		return crejsText;
+	}
+	
+	// Os nomes dos elementos são seus números começando de 0 (zero).
+	// Por exemplo: 0, 1, 2, 3...
+	p.cItem = function(nameItem, x, y)
+	{ 
+		var itemBitmap = new createjs.Bitmap("asset/images/ele"+ nameItem + ".jpg");
+		itemBitmap.x = x;
+		itemBitmap.y = y;
+		itemBitmap.name = nameItem;
+		
+		return itemBitmap;
+	}
+	
+	p.rigthClick = function(event)
 	{
-		alert("criando");
+	}
+	
+	p.wrongClick = function()
+	{
 	}
 	
 	window.GameplayScreen = GameplayScreen;
